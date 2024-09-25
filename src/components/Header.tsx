f7d180b822a1306562e355171c134c3e5e014119
Zülal <zllnbn1997@gmail.com>
2024-09-25 16:13:38 +0300

    add back icon to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,47 @@
-import { Box } from "@mui/material";
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 export const Header = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
-      <Link href="/">
-        <Image
-          src="https://logos-world.net/wp-content/uploads/2022/01/Rick-And-Morty-Symbol.png}"
-          alt="rick and morty"
-          priority
-          width={280}
-          height={120}
-        />
-      </Link>
-    </Box>
+    <Container>
+      <Box
+        sx={{ display: "flex", justifyContent: "center", position: "relative" }}
+      >
+        {pathname !== "/" && pathname !== "/locations/page/1" && (
+          <IconButton
+            onClick={() => router.back()}
+            sx={{
+              position: "absolute",
+              left: 0,
+              top: "50%",
+              transform: "translateY(-50%)",
+              color: "black",
+              border: "2px solid black",
+            }}
+          >
+            <ArrowBackIosNewIcon />
+          </IconButton>
+        )}
+        <Link href="/">
+          <Image
+            src="https://logos-world.net/wp-content/uploads/2022/01/Rick-And-Morty-Symbol.png}"
+            alt="rick and morty"
+            priority
+            width={280}
+            height={120}
+          />
+        </Link>
+      </Box>
+    </Container>
   );
 };
