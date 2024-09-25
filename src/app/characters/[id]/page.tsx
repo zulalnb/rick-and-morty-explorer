6410131f0f4dceff6e5fae1b632ef3e0511d4d81
Zülal <zllnbn1997@gmail.com>
2024-09-25 13:35:52 +0300

    add other characters to character details page

--- src/app/characters/[id]/page.tsx
+++ src/app/characters/[id]/page.tsx
@@ -6,6 +6,8 @@ import Typography from "@mui/material/Typography";
 import CircleIcon from "@mui/icons-material/Circle";
 import { Character } from "@/types/character";
 import { Location } from "@/types/location";
+import { getRandomItems } from "@/lib/utils";
+import Link from "next/link";
 
 const getLocation = async (url: string): Promise<Location> => {
   const res = await fetch(url);
@@ -21,11 +23,47 @@ const getCharacterDetail = async (id: number): Promise<Character> => {
   return data;
 };
 
+const getOtherCharacters = async (
+  characterIds: number[],
+  status: string
+): Promise<Character[]> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_API_URL}/character/${characterIds}`
+  );
+  const characters: Character[] = await res.json();
+  const filteredCharacters = characters.filter(
+    (character) => character.status === status
+  );
+  const otherCharacters: Character[] =
+    filteredCharacters.length > 2
+      ? getRandomItems(filteredCharacters)
+      : filteredCharacters;
+
+  return otherCharacters;
+};
+
 export default async function Page({ params }: { params: { id: number } }) {
   const character = await getCharacterDetail(params.id);
   const location = character
     ? await getLocation(character.location.url)
-    : { dimension: "" };
+    : { residents: [], dimension: "" };
+
+  const characterIds = character
+    ? location.residents
+        .filter((resident) => {
+          const parts = resident.split("/");
+          return parseInt(parts[parts.length - 1], 10) !== Number(params.id);
+        })
+        .map((resident) => {
+          const parts = resident.split("/");
+          return parseInt(parts[parts.length - 1], 10);
+        })
+    : [];
+
+  const otherCharacters = await getOtherCharacters(
+    characterIds,
+    character.status
+  );
 
   return (
     <main>
@@ -75,12 +113,75 @@ export default async function Page({ params }: { params: { id: number } }) {
                     </Typography>
                   </Box>
                   <Typography sx={{ fontStyle: "italic", fontSize: 18 }}>
-                    {location.dimension}
+                    {character.location.name}
                   </Typography>
                 </Box>
                 <Typography sx={{ fontStyle: "italic" }}>
                   {character.type || "-"} / {character.gender}
                 </Typography>
+              </Box>
+            </Grid>
+          )}
+          {otherCharacters.length > 0 && (
+            <Grid size={6}>
+              <Box>
+                <Typography sx={{ fontWeight: "bold", fontSize: 24, mb: 4 }}>
+                  Other Characters
+                </Typography>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
+                  {otherCharacters.map((character) => (
+                    <Box
+                      component={Link}
+                      href={`/characters/${character.id}`}
+                      key={character.id}
+                      sx={{ display: "flex", gap: 2 }}
+                    >
+                      <Box
+                        sx={{
+                          position: "relative",
+                          aspectRatio: 1 / 1,
+                          width: "20%",
+                        }}
+                      >
+                        <Image
+                          src={character.image}
+                          alt={character.name}
+                          fill
+                          style={{
+                            position: "absolute",
+                            top: 0,
+                            right: 0,
+                            bottom: 0,
+                            left: 0,
+                          }}
+                        />
+                      </Box>
+                      <Box sx={{ display: "flex", flexDirection: "column" }}>
+                        <Typography
+                          sx={{
+                            fontWeight: "bold",
+                            color: "gray",
+                            fontSize: 24,
+                          }}
+                        >
+                          {character.name}
+                        </Typography>
+                        <Typography sx={{ fontStyle: "italic", fontSize: 18 }}>
+                          {character.location.name}
+                        </Typography>
+                        <Typography
+                          sx={{
+                            fontStyle: "italic",
+                            marginTop: "auto",
+                            marginBottom: 0,
+                          }}
+                        >
+                          {character.type || "-"} / {character.gender}
+                        </Typography>
+                      </Box>
+                    </Box>
+                  ))}
+                </Box>
               </Box>
             </Grid>
           )}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,11 @@ export function paginateItems<T>(
 
   return { paginatedArray, totalPages };
 }
+
+export const getRandomItems = <T>(
+  array: T[],
+  numberOfItems: number = 2
+): T[] => {
+  const shuffledArray = [...array].sort(() => 0.5 - Math.random()); // Shuffle the array
+  return shuffledArray.slice(0, numberOfItems); // Return the first `numberOfItems` from the shuffled array
+};
