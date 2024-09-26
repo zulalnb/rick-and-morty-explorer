835f51798e7fc796c7c672fbe98c7dc6c974beb4
Zülal <zllnbn1997@gmail.com>
2024-09-26 15:04:46 +0300

    enable trailing slash

--- src/components/LocationSkeleton.jsx
+++ src/components/LocationSkeleton.jsx
@@ -7,7 +7,11 @@ export const LocationSkeleton = () => {
   return (
     <Grid
       size={{ xs: 12, md: 6 }}
-      sx={{ border: "1px solid black", borderRadius: 4, padding: 2 }}
+      sx={{
+        border: "1px solid rgba(0, 0, 0, 0.2)",
+        borderRadius: 4,
+        padding: 2,
+      }}
     >
       <Skeleton variant="text" sx={{ fontSize: "1.75rem" }} />
       <Skeleton variant="text" sx={{ fontSize: "1.75rem" }} />
