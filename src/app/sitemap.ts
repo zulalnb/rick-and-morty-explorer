6ecefab7beaac2cf5ed3e6bf82ef6c7745f0b5a9
Zülal Nebin <zllnbn1997@gmail.com>
2025-11-02 18:43:50 +0300

    sitemap fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,6 @@
 import type { MetadataRoute } from "next";
 import { BASE_API_URL, BASE_URL } from "@/lib/constants";
-import { paginateItems } from "@/lib/utils";
-import { Location, LocationAPIResponse } from "@/types/location";
+import { Location } from "@/types/location";
 import { Character } from "@/types/character";
 
 async function fetchAll<T>(apiUrl: string): Promise<T[]> {
@@ -29,40 +28,11 @@ async function fetchAll<T>(apiUrl: string): Promise<T[]> {
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticRoutes = [{ url: `${BASE_URL}/`, lastModified: new Date() }];
 
-  const firstLocationPage = await fetch(`${BASE_API_URL}/location`).then(
-    (res) => res.json() as Promise<LocationAPIResponse>
-  );
-
-  const totalLocationPages = firstLocationPage.info.pages;
-  const locationListPages = Array.from(
-    { length: totalLocationPages - 1 },
-    (_, i) => ({
-      url: `${BASE_URL}/page/${i + 2}/`,
-      lastModified: new Date(),
-    })
-  );
-
   const allLocations = await fetchAll<Location>(`${BASE_API_URL}/location`);
-  const locationRoutes = allLocations.flatMap((location) => {
-    const routes = [
-      {
-        url: `${BASE_URL}/locations/${location.id}/characters/`,
-        lastModified: new Date(location.created),
-      },
-    ];
-
-    const characterCount = location.residents.length;
-    if (characterCount > 0) {
-      const { totalPages } = paginateItems(location.residents);
-      for (let i = 2; i <= totalPages; i++) {
-        routes.push({
-          url: `${BASE_URL}/locations/${location.id}/characters/page/${i}/`,
-          lastModified: new Date(location.created),
-        });
-      }
-    }
-    return routes;
-  });
+  const locationRoutes = allLocations.map((location) => ({
+    url: `${BASE_URL}/locations/${location.id}/characters/`,
+    lastModified: new Date(location.created),
+  }));
 
   const allCharacters = await fetchAll<Character>(`${BASE_API_URL}/character`);
   const characterRoutes = allCharacters.map((character) => ({
@@ -70,10 +40,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     lastModified: new Date(character.created),
   }));
 
-  return [
-    ...staticRoutes,
-    ...locationListPages,
-    ...locationRoutes,
-    ...characterRoutes,
-  ];
+  return [...staticRoutes, ...locationRoutes, ...characterRoutes];
 }
